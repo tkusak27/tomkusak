306779fb6d335f08631da89912daac738b29b1b5
Tom Kusak <tkusak@nd.edu>
2024-02-21 16:05:20 +0000

    need to fix css

--- example/models.py
+++ example/models.py
@@ -1,6 +1,16 @@
 from django.db import models
 import datetime
 
+class Page(models.Model):
+    title = models.CharField(max_length=60)
+    permalink = models.CharField(max_length=12, unique=True)
+    update_date = models.DateTimeField('Last Updated')
+    create_date = models.DateField('First Published', default = datetime.date.today)
+    bodytext = models.TextField('Page Content', blank=True)
+
+    def __str__(self):
+        return self.title
+
 class Category(models.Model):
     name = models.CharField(max_length=30)
 
