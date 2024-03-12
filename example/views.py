ac51405aae84065c2c2d96c78c37273b00a6c71c
Tom Kusak <tkusak@nd.edu>
2024-03-12 18:51:55 +0000

    got models working

--- example/views.py
+++ example/views.py
@@ -1,16 +1,36 @@
 # \simple_test_root\pages\views.py
 from django.shortcuts import render
+from . models import Page
 
-def index(request, pagename=''):
-    context = {}
+def index(request):
+    pg = Page.objects.get(permalink='/')
+    context = {
+        'title': pg.title,
+        'content': pg.bodytext,
+        'last_updated': pg.update_date,
+        'page_list': Page.objects.all(),
+    }
     return render(request, 'index.html', context)
 
 def projects(request):
-    context = {}
+    pg = Page.objects.get(permalink='/projects')
+    context = {
+        'title': pg.title,
+        'content': pg.bodytext,
+        'last_updated': pg.update_date,
+        'page_list': Page.objects.all(),
+    }
     return render(request, 'projects.html', context)
 
+
 def about(request):
-    context = {}
+    pg = Page.objects.get(permalink='/about')
+    context = {
+        'title': pg.title,
+        'content': pg.bodytext,
+        'last_updated': pg.update_date,
+        'page_list': Page.objects.all(),
+    }
     return render(request, 'about.html', context)
 
 
